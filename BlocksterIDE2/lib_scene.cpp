2ef7c0fc8421477109ff23539e4f0d14bb62055d
Petter Tomner <petter.vilhelm@gmail.com>
2017-06-27 00:10:07 +0200

    Added some lines

--- BlocksterIDE2/lib_scene.cpp
+++ BlocksterIDE2/lib_scene.cpp
@@ -1,6 +1,11 @@
 #include "lib_scene.h"
 
-LibScene::LibScene()
+LibScene::LibScene(std::string lib_path)
 {
+    this->lib.parseFolderTree(lib_path);
+}
 
+void LibScene::reparseLib()
+{
+    this->lib.parseFolderTree(lib.root_dir);
 }
--- BlocksterIDE2/lib_scene.h
+++ BlocksterIDE2/lib_scene.h
@@ -1,11 +1,22 @@
 #ifndef LIB_SCENE_H
 #define LIB_SCENE_H
 
+#include <QObject>
+#include <QGraphicsScene>
 
+#include "b_lib_tree.h"
+#include <string>
 class LibScene : public QGraphicsScene
 {
+    Q_OBJECT
 public:
-    LibScene();
+    LibScene(std::string lib_path);
+
+    bster::b_lib_tree lib;
+
+public slots:
+    void reparseLib();
+
 };
 
 #endif // LIB_SCENE_H
